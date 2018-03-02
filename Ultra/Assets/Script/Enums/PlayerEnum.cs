8291a80990eb3840183db3a182ba032fb76399b2
Marek <thenjyon@googlemail.com>
2018-03-02 19:16:17 +0100

    IDK

---
+++ Ultra/Assets/Script/Enums/PlayerEnum.cs
@@ -0,0 +1,6 @@
+﻿public enum PlayerEnum
+{
+    NotAssigned,
+    PlayerOne,
+    PlayerTwo
+};
---
+++ Ultra/Assets/TEMP/TEST/MyCharacter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCharacter : MonoBehaviour
+{
+    PlayerEnum playerEnum = PlayerEnum.NotAssigned;
+
+    void Awake()
+    {
+       
+    }
+
+    void Start ()
+    {
+		
+	}
+
+	void Update ()
+    {
+		
+	}
+    
+
+    void Spawn()
+    {
+
+    }
+
+    void Respawn()
+    {
+
+    }
+
+    void Dead()
+    {
+
+    }
+}
