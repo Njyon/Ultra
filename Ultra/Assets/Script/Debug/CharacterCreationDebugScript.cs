8d16ca7605a31fd695df1b2e6e02589b16113887
Unknown <thenjyon@googlemail.com>
2018-04-14 14:02:07 +0200

    Debug Modus
    Use The Debug Script to Only spawn One Character and test features

---
+++ Ultra/Assets/Script/Debug/CharacterCreationDebugScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCreationDebugScript : MonoBehaviour
+{
+    [Header("The Character That Needs to be Debuged")]
+    public GameObject debugCharacter;
+    [Header("The Location where the Character gets Instatiated")]
+    public GameObject spawnLocation;
+    [Header("Switch The Player Enum if no Controll")]
+    public PlayerEnum playerEnum;
+
+    GameObject debugPlayer;
+
+    private void Start()
+    {
+        Initiate();
+    }
+
+    void Initiate()
+    {
+        debugPlayer = Instantiate(debugCharacter, spawnLocation.gameObject.transform.position, spawnLocation.gameObject.transform.rotation);
+        debugPlayer.GetComponent<MyCharacter>().playerEnum = playerEnum;
+        debugPlayer.GetComponent<MyCharacter>().Posses();
+    }
+}
