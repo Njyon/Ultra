1123604b6a2ce0d185f6c9796ebf1ba07e73f898
David Schornsheim <david.schornsheim@gmx.us>
2018-07-02 23:56:32 +0200

    Landing-Timeline-Layer added (currently triggered with random intensity)

--- Ultra/Assets/AudioEvents.cs
+++ Ultra/Assets/AudioEvents.cs
@@ -44,6 +44,9 @@ public class AudioEvents : MonoBehaviour {
             case EventState.Fall:
                 break;
             case EventState.Landing:
+                var rand = Random.Range(0f, 1f);
+                Debug.Log(rand);
+                Fabric.EventManager.Instance.SetParameter("Landing", "LandingImpact", rand, this.gameObject);
                 Fabric.EventManager.Instance.PostEvent("Landing", this.gameObject);
                 break;
             case EventState.Dodge:
