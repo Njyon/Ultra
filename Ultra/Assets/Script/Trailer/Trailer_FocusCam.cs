8781fb8bc434cfbcee40f9e7da1c88d4067fccd0
Unknown <thenjyon@googlemail.com>
2018-08-08 13:16:26 +0200

    Trailer Footage

---
+++ Ultra/Assets/Script/Trailer/Trailer_FocusCam.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trailer_FocusCam : MonoBehaviour
+{
+    public MultiTargetCamera cam;
+    public Transform go;
+    public Transform go2;
+
+    private void Awake()
+    {
+        cam.AddTarget(go);
+        cam.AddTarget(go2);
+    }
+}
